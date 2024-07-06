9f1621dac8e7c9729ca05a368fc60f7ba2de3558
reisjean <jeanpreis@gmail.com>
2024-07-06 20:08:44 -0300

    feat: Add user questions and assistant responses to chat route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,3 +15,25 @@ export async function POST(req: Request) {
 
   return result.toAIStreamResponse()
 }
+
+messages: [
+  {
+    role: "user",
+    content: "Onde estão localizados os escritórios da FinTechX?",
+  },
+  {
+    role: "assistant",
+    content:
+      "Os escritórios da FinTechX estão localizados em São Paulo, Madri e Nova York. Posso te ajudar com mais alguma coisa?",
+  },
+  { role: "user", content: "qual a história da empresa?" },
+  {
+    role: "assistant",
+    content:
+      "A FinTechX foi fundada por Gandalf e Dumbledore em 1876 com a missão de democratizar o acesso a serviços financeiros de qualidade para todos os brasileiros, oferecendo soluções inovadoras e acessíveis. A empresa busca constantemente aprimorar seus serviços e tecnologias para atender às necessidades do mercado financeiro de forma moderna e ágil. Posso te ajudar com mais alguma coisa?",
+  },
+  {
+    role: "user",
+    content: "quais os tipos de investimentos disponíveis?",
+  },
+]
