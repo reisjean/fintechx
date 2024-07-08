a4ed55188228f5cce5903a782064e850720b7f68
reisjean <jeanpreis@gmail.com>
2024-07-08 14:02:04 -0300

    feat: Update navigation animation in Navigation component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -40,9 +40,9 @@ export function Navigation() {
       {current === 'profile' && (
         <motion.div
           key="profile"
-          initial={{ x: 100, opacity: 0 }}
+          initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
-          exit={{ x: 100, opacity: 0 }}
+          exit={{ x: -100, opacity: 0 }}
           transition={{ duration: 0.15 }}
           className="h-full"
         >
